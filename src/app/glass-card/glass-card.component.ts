bab78a491a2907c77942c6ae88b8cbbff0a8265c
LinaRagauskaiteGSI <lina.ragauskaite@geospatial-insight.com>
2019-01-19 20:08:04 +0000

    Added countdown glass card

---
+++ src/app/glass-card/glass-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-glass-card',
+  templateUrl: './glass-card.component.html',
+  styleUrls: ['./glass-card.component.scss']
+})
+export class GlassCardComponent {
+  days = 5
+  hours = 23
+  minutes = 59
+}
