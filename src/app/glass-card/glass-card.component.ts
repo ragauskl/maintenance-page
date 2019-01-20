0b756b476ed571aa39db534c26b6f7c3090c722b
LinaRagauskaiteGSI <lina.ragauskaite@geospatial-insight.com>
2019-01-20 21:18:14 +0000

    Updated end date and calculation

--- src/app/glass-card/glass-card.component.ts
+++ src/app/glass-card/glass-card.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core'
-
+import * as moment from 'moment'
 @Component({
   selector: 'app-glass-card',
   templateUrl: './glass-card.component.html',
   styleUrls: ['./glass-card.component.scss']
 })
 export class GlassCardComponent {
-  days = 5
-  hours = 23
-  minutes = 59
+  readonly endDate = moment('01/04/2019 21:00', 'DD/MM/YYYY HH:mm')
+  days = 0
+  hours = 0
+  minutes = 0
+  constructor () {
+    setInterval(() => {
+      const left = moment.duration(this.endDate.diff(moment()))
+      this.days = Math.floor(left.asDays())
+      this.hours = Math.floor(left.asHours() % 24)
+      this.minutes = Math.floor(left.asMinutes() % 60)
+    }, 1000)
+  }
 }
