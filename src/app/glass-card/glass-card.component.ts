e18ab1dfcd50b4c17f1f4c67a6515cf743a63218
LinaRagauskaiteGSI <lina.ragauskaite@geospatial-insight.com>
2019-01-21 08:53:36 +0000

    Moved counter to service

--- src/app/glass-card/glass-card.component.ts
+++ src/app/glass-card/glass-card.component.ts
@@ -1,21 +1,11 @@
 import { Component } from '@angular/core'
-import * as moment from 'moment'
+import { CountdownService } from '../services/countdown.service'
 @Component({
   selector: 'app-glass-card',
   templateUrl: './glass-card.component.html',
   styleUrls: ['./glass-card.component.scss']
 })
 export class GlassCardComponent {
-  readonly endDate = moment('01/04/2019 21:00', 'DD/MM/YYYY HH:mm')
-  days = 0
-  hours = 0
-  minutes = 0
-  constructor () {
-    setInterval(() => {
-      const left = moment.duration(this.endDate.diff(moment()))
-      this.days = Math.floor(left.asDays())
-      this.hours = Math.floor(left.asHours() % 24)
-      this.minutes = Math.floor(left.asMinutes() % 60)
-    }, 1000)
-  }
+
+  constructor (public countdownService: CountdownService) {}
 }
---
+++ src/app/services/countdown.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core'
+import * as moment from 'moment'
+@Injectable({
+  providedIn: 'root'
+})
+export class CountdownService {
+  readonly endDate = moment('01/04/2019 21:00', 'DD/MM/YYYY HH:mm')
+  days = 0
+  hours = 0
+  minutes = 0
+  constructor () {
+    setInterval(() => {
+      const left = moment.duration(this.endDate.diff(moment()))
+      this.days = Math.floor(left.asDays())
+      this.hours = Math.floor(left.asHours() % 24)
+      this.minutes = Math.floor(left.asMinutes() % 60)
+    }, 1000)
+  }
+
+  get countdownActive (): boolean {
+    return !!(this.days || this.hours || this.minutes)
+  }
+}
